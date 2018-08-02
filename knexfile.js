5170e047ea4833c22d8c1764765f92de0733d74e
hyperrex <mrwendell@gmail.com>
2018-08-02 09:36:34 -0700

    Add testing and production db to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,6 @@
 // Define DB connections for different environments
 module.exports = {
+
   development: {
     client: 'pg',
     connection: 'postgres://localhost/voter-status-dev'
@@ -8,5 +9,27 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
+  },
+
+  testing: {
+    client: 'pg',
+    connection: 'postgres://localhost/voter-status-test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    }
   }
 }
