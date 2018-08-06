27953ab189ce106284ba3870e162a7ac49a37b85
hyperrex <mrwendell@gmail.com>
2018-08-06 10:27:28 -0700

    Add user table migration and seed file

---
+++ migrations/20180802214223_users.js
@@ -0,0 +1,27 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('users', function(table) {
+      table.increments('id').primary();
+      table.integer('remote_id')
+      table.string('email').notNullable().unique('email');
+      table.string('password').notNullable();
+      //replace above line with: table.specificType('hashedPassword', 'char(60)').notNullable();
+      table.string('birthdate');
+      table.integer('last_four_ssn'); // later, also add in CDL
+      table.boolean('voter_status');
+      table.string('registration_date');
+      table.boolean('vote_by_mail');
+      table.string('party');
+      table.string('language');
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('users')
+  ]);
+};
---
+++ seeds/001_users.js
@@ -0,0 +1,46 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { id: 1,
+          remote_id: '97583',
+          email: 'guy.fleegman@NSEA-Protector.com',
+          password: 'KlydenIsH0t',
+          birthdate: '11/5/1968',
+          last_four_ssn: 5567,
+          voter_status: 1,
+          registration_date: '6/5/2010',
+          vote_by_mail: 0,
+          party: 'Republican',
+          language: 'English'
+        },
+        { id: 2,
+          remote_id: 0,
+          email: 'Commander.Bortus@USS-Orville.com',
+          password: 'KlydenIsH0t',
+          birthdate: '5/23/1977',
+          last_four_ssn: 2592,
+          voter_status: 1,
+          registration_date: '3/7/2016',
+          vote_by_mail: 1,
+          party: 'No Preference',
+          language: 'Moclan'
+        },
+        { id: 3,
+          remote_id: '334561',
+          email: 'Doctor@tardis.net',
+          password: 'WhoMe???',
+          birthdate: '4/18/1971',
+          last_four_ssn: 9275,
+          voter_status: 1,
+          registration_date: '1/8/2008',
+          vote_by_mail: 0,
+          party: 'Democratic',
+          language: 'English'
+        }
+      ]);
+    });
+};
