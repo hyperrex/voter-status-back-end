3801e28e3aea3490afcdc4b8a98019340015f38c
hyperrex <mrwendell@gmail.com>
2018-08-20 10:05:36 -0700

    Update users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const knex = require('../knex');
 
-/* GET users listing. */
+// GET users listing
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  knex('users')
+  .then((usersArray) => {
+    res.send(usersArray)
+  })
+})
+
+// POST name
+router.post('/', function(req, res, next) {
+  console.log('fnord')
+  // send this data to the scraper and get back a Response
+  knex('users')
+  .where({email: 'guy.fleegman@NSEA-Protector.com'})
+  .then()
+  })
+})
 
 module.exports = router;
